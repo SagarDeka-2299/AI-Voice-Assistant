5ef0692e345e950e3a2656bc3e74320f9becfdf6
SagarDeka-2299 <denimstark2003@gmail.com>
2025-09-07 14:17:01 +0000

    fixed twilio server to support new version VoiceAssistant class

--- twilio_server.py
+++ twilio_server.py
@@ -8,10 +8,10 @@ import base64
 import json
 import os
 import uvicorn
-from Assistant import VoiceAssistant
+from Assistant import VoiceAssistant, ASR_Worker, TTS_Worker
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, BaseMessage, AIMessage
-from typing import List, Optional,AsyncGenerator
+from typing import List, Optional,AsyncGenerator, Callable
 from numpy.typing import NDArray
 import numpy as np
 import torch
@@ -24,6 +24,64 @@ from TTS.config.shared_configs import BaseDatasetConfig
 from dotenv import load_dotenv
 load_dotenv()
 
+
+class Whisper_ASR_Worker(ASR_Worker):
+    def init_asr(self) -> Callable[[NDArray[np.float32]], str]:
+        from faster_whisper import WhisperModel
+        model = WhisperModel(
+            "large-v3" if torch.cuda.is_available() else "small", 
+            device="cuda" if torch.cuda.is_available() else "cpu", 
+            compute_type= "float16" if torch.cuda.is_available() else "int8"
+        )
+        print("✅ ASR model loaded 🗣️ -> 🔤")
+        def inference(audio_np: NDArray[np.float32])->str:
+            segments, _ = model.transcribe(
+                audio_np,
+                beam_size=5,
+                word_timestamps=False,
+                task="transcribe",
+                language="en"
+            )
+            full_text = "".join(segment.text for segment in segments).strip()
+            return full_text
+        return inference
+class XTTS_Worker(TTS_Worker):
+    def init_tts(self) -> Callable[[str], NDArray[np.float32]]:
+        from TTS.api import TTS #0.22.0
+        from TTS.tts.configs.xtts_config import XttsConfig
+        from TTS.tts.models.xtts import XttsAudioConfig, XttsArgs
+        from TTS.config.shared_configs import BaseDatasetConfig
+        import torch
+        torch.serialization.add_safe_globals([XttsConfig, XttsAudioConfig, BaseDatasetConfig, XttsArgs])
+        tts= TTS("tts_models/multilingual/multi-dataset/xtts_v2").to("cuda")
+        print("✅ TTS model loaded 🔤 -> 🗣️")
+        def Speech_Generator(txt:str)->NDArray[np.float32]:
+            return np.array(tts.tts(
+                        text=txt,
+                        speaker_wav="sample_voice.wav",
+                        language="en",
+                    ),dtype=np.float32)
+        return Speech_Generator
+class Kokoro_TTS_Worker(TTS_Worker):
+    def init_tts(self) -> Callable[[str], NDArray[np.float32]]:
+        from kokoro import KPipeline
+        pipe = KPipeline(lang_code="a")
+        print("✅ TTS model loaded 🔤 -> 🗣️")
+        def synthesise(txt:str)->NDArray[np.float32]:
+            gen = pipe(txt, voice="af_bella")
+            audio_chunks = []
+            for segment in gen:
+                # in the latest repo, segment.audio is already a torch.Tensor
+                wav = segment.audio
+                if wav!=None:
+                    audio_chunks.append(wav.numpy())
+            # concatenate all chunks into one waveform
+            audio_np = np.concatenate(audio_chunks)
+            return audio_np
+        return synthesise
+        
+
+
 class GPTVoiceAssistant(VoiceAssistant):
     def __init__(self):
         super().__init__(
@@ -32,71 +90,65 @@ class GPTVoiceAssistant(VoiceAssistant):
             output_sample_rate_khz=8,
             vad_chunk_duration_ms=32,
             asr_chunk_duration_ms=30000,
-            output_chunk_duration_ms=20
+            output_chunk_duration_ms=20,
+            asr_worker_initializer=Whisper_ASR_Worker,
+            tts_worker_initializer=XTTS_Worker if torch.cuda.is_available() else Kokoro_TTS_Worker
         )
         self.message_history: List[BaseMessage]=[
             SystemMessage("""
                 Your name is Megan.
-                You are a smart, brilliant, healpful voice assistant. 
+                You are a smart, brilliant, healpful voice assistant's brain. 
                 The upcoming user queries are detected from user speech, 
                 you answer to these user queries in text, in a conversation style.
                 Your answer will be converted to speech.
                 So make it sound natural. Keep user engaged.
+                In case the user query is not complete, just short statement, reply with hmm, mhmm, uhmmm, u r right...etc
             """),
         ]
         self.stream_sid:Optional[str]=None
         self.websocket:Optional[WebSocket]=None
-        self.llm = ChatOpenAI(model="gpt-4o", streaming=True)
-        self.vad=load_silero_vad()
-        self.asr=WhisperModel(
-            "large-v3", 
-            device="cuda", 
-            compute_type= "float16"
-        )
-        torch.serialization.add_safe_globals([XttsConfig, XttsAudioConfig, BaseDatasetConfig, XttsArgs])
-        self.tts= TTS("tts_models/multilingual/multi-dataset/xtts_v2").to("cuda")
-        self.tts_sample_path="sample_voice.wav"
+        self.llm = ChatOpenAI(model="gpt-4o-mini", streaming=True)
 
-    async def  On_Interruption(self):
-        print("interrupted")
 
-    async def Thinking_Generator(self, query:str)-> AsyncGenerator[str, None]:
+    
+    def on_user_query(self, query:str):
+        if not query or not len(query):
+            return
         self.message_history.append(HumanMessage(content=query))
+        if not len(self.message_history) or not isinstance(self.message_history[-1], HumanMessage) or not isinstance(self.message_history[-1].content, str):
+            self.message_history.append(HumanMessage(content=query))
+        else:
+            self.message_history[-1]=HumanMessage(content=self.message_history[-1].content+query)
+        
+    def init_vad(self)->Callable[[NDArray[np.float32]],bool]:
+        """This method should be overridden to load the VAD model and return the inference method that takes audio chunk float32 and returns boolean"""
+        vad=load_silero_vad()
+        print("✅ VAD model loaded 🔊 -🔎-> 🗣️")
+        def Detect_Speech(chunk: NDArray[np.float32])->bool:
+            audio_tensor = torch.from_numpy(chunk)     
+            speech_prob = vad(audio_tensor, self.input_sample_rate_khz*1000).item()
+            return speech_prob>= 0.8
+        return Detect_Speech
+
+
+
+    async def On_Brain_Response(self, thought:str):
+        if not len(thought):
+            return
+        if not len(self.message_history) or not isinstance(self.message_history[-1], AIMessage) or not isinstance(self.message_history[-1].content, str):
+            self.message_history.append(AIMessage(content=thought))
+        else:
+            self.message_history[-1]=AIMessage(content=self.message_history[-1].content+thought)
+
+    async def On_Interruption(self):
+        print("interrupted")
+        
+
+    async def Thinking_Generator(self)-> AsyncGenerator[str, None]:
         async for chunk in self.llm.astream(self.message_history):
             if isinstance(chunk.content,str):
+                print(f"🧠: {chunk.content}")
                 yield chunk.content
-
-    async def Speech_Generator(self,txt:str)->AsyncGenerator[NDArray[np.float32],None]:
-        yield np.array(self.tts.tts(
-                    text=txt,
-                    speaker_wav=self.tts_sample_path,
-                    language="en",
-                ),dtype=np.float32)
-    
-
-    async def On_Brain_Response(self, res: str):
-        if not len(self.message_history) or not isinstance(self.message_history[-1], AIMessage) or not isinstance(self.message_history[-1].content, str):
-            self.message_history.append(AIMessage(content=res))
-        else:
-            self.message_history[-1]=AIMessage(content=self.message_history[-1].content+res)
-    
-
-    async def Detect_Speech(self, chunk: NDArray[np.float32])->bool:
-        audio_tensor = torch.from_numpy(chunk)     
-        speech_prob = self.vad(audio_tensor, self.input_sample_rate_khz*1000).item()
-        return speech_prob>= 0.8
-
-    async def Transcribe_Speech(self, chunk: NDArray[np.float32])->str:
-        segments, _ = self.asr.transcribe(
-            chunk,
-            beam_size=5,
-            word_timestamps=False,
-            task="transcribe",
-            language="en"
-        )
-        full_text = "".join(segment.text for segment in segments).strip()
-        return full_text
-
 
     async def Handle_Output_Audio_i16_Bytes(self, chunk: bytes) -> None:
         if chunk and self.websocket:
@@ -113,6 +165,7 @@ class GPTVoiceAssistant(VoiceAssistant):
             }
             await self.websocket.send_text(json.dumps(media_message))
             await sleep(0.02)
+
 
 
 from contextlib import asynccontextmanager
